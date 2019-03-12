b63b492075004363b1768d364fd00a66c63e986c
Luis Inzunza <luisinzu08@gmail.com>
2019-03-12 03:28:37 -0700

    spacing innerhtml

--- boyorgirl.js
+++ boyorgirl.js
@@ -4,8 +4,8 @@ var girl = 0;
 function refreshResults () {
   var results = document.getElementById('results');
   results.innerHTML = 'TOTAL: ' + (boy + girl);
-  results.innerHTML += '<br/>BOY: ' + boy;
-  results.innerHTML += '<br/>GIRL: ' + girl;
+  results.innerHTML += ' BOY: ' + boy;
+  results.innerHTML += ' GIRL: ' + girl;
 }
 
 document.getElementById('boy-button').addEventListener('click', function () {
